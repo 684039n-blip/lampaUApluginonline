b0da876f7fe0d82af6ccc183001e7bcbad845c22
684039n-blip <684039n@gmail.com>
2026-01-08 23:25:54 +0200

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,4 +1,22 @@
 (function () {
     'use strict';
-    Lampa.Utils.putScriptAsync(['https://684039n-blip.github.io/lampaUAplugin/BanderaOnline.js'], function () { });
+    
+    // Чекаємо поки Lampa повністю завантажиться
+    setTimeout(function() {
+        if (!window.Lampa || !Lampa.Utils) {
+            console.error('Lampa не знайдена!');
+            return;
+        }
+        
+        console.log('=== UaFlix Plugin Injector ===');
+        
+        // Завантажуємо основний плагін
+        Lampa.Utils.putScriptAsync([
+            'https://684039n-blip.github.io/lampaUAplugin/UaflixPlugin.js'
+        ], function() {
+            console.log('UaFlix Plugin завантажено!');
+        });
+        
+    }, 3000); // Чекаємо 3 секунди
+    
 })();
